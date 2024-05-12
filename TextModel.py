d20c3a3c206dc56cf4b6fe29408153a724941e4f
orl <orl@curve.tech>
2024-05-12 12:56:09 +0300

    done

--- TextModel.py
+++ TextModel.py
@@ -1,3 +1,5 @@
+import pickle
+
 from sklearn.metrics import accuracy_score, classification_report
 import librosa
 import numpy as np
@@ -11,10 +13,12 @@ from torch.nn.utils.rnn import pad_sequence
 from collections import Counter
 from nltk.tokenize import word_tokenize
 import nltk
+
 nltk.download('punkt')
 import gensim.downloader as api
 import re
 import xgboost as xgb
+
 
 class TextCleaner:
     def __init__(self, language='en'):
@@ -41,6 +45,7 @@ class TextCleaner:
         res = target.replace({'negative': 0, 'neutral': 1, 'positive': 2})
         return res
 
+
 # Tokenization and Vocabulary Building
 def build_vocab(data):
     counter = Counter()
@@ -50,6 +55,7 @@ def build_vocab(data):
     vocab = {word: i + 1 for i, (word, _) in enumerate(counter.items())}
     vocab['<pad>'] = 0
     return vocab
+
 
 def collate_batch(batch):
     label_list, text_list, lengths = [], [], []
@@ -151,21 +157,24 @@ def extract_features(waveform, sr):
     return features
 
 
-def process_audio_data(audio_dict, sample_rate):
-    """
-    Process each audio scene, extract features, and compile them into a single DataFrame.
-    """
-    feature_list = []
+def sentence_to_vec(df, embedding_model):
+    vec_list = []
+    for index, row in df.iterrows():
+        sentence = row['tokens']  # Directly using the 'tokens' column
+        word_vectors = []
+        for word in sentence.split():
+            try:
+                word_vectors.append(embedding_model[word])
+            except KeyError:
+                continue  # Skip words not in the vocabulary
+        if word_vectors:
+            vec_list.append(np.mean(word_vectors, axis=0))
+        else:
+            vec_list.append(np.zeros(100))  # Assuming 100 dimensional embeddings
 
-    for scene_id, data in audio_dict.items():
-        waveform = data['waveforms'][0].numpy()  # Ensure waveform is a NumPy array
-        features = extract_features(waveform, sample_rate)
-        features['scene_id'] = scene_id  # Add scene_id to the features dictionary
-        feature_list.append(features)
-
-    # Create a DataFrame from the list of feature dictionaries
-    combined_features_df = pd.DataFrame(feature_list)
-    return combined_features_df
+    # Create a DataFrame from the list of vectors and preserve the original index
+    vec_df = pd.DataFrame(vec_list, index=df.index)
+    return vec_df
 
 
 if __name__ == '__main__':
@@ -180,24 +189,63 @@ if __name__ == '__main__':
     df_train = df_train.dropna(subset=['tokens'])
     df_dev = df_dev.dropna(subset=['tokens'])
     df_test = df_test.dropna(subset=['tokens'])
+    # set key
+    df_train['scene_id'] = 'dia' + df_train['Dialogue_ID'].astype(str) + '_' + 'utt' + df_train['Utterance_ID'].astype(
+        str)
+    df_test['scene_id'] = 'dia' + df_test['Dialogue_ID'].astype(str) + '_' + 'utt' + df_test['Utterance_ID'].astype(str)
+    df_train = df_train.set_index('scene_id')
+    df_test = df_test.set_index('scene_id')
+
     # set target
     df_train['labels'] = text_cleaner.set_target(df_train['Sentiment'])
     df_dev['labels'] = text_cleaner.set_target(df_dev['Sentiment'])
     df_test['labels'] = text_cleaner.set_target(df_test['Sentiment'])
 
     glove_model = api.load("glove-wiki-gigaword-100")
-    text_test = df_test['tokens'].values.tolist()
-    sentence_vectors_test = sentence_to_vec(text_test, glove_model)
-    text_train = df_train['tokens'].values.tolist()
-    sentence_vectors_train = sentence_to_vec(text_train, glove_model)
-    X_train = pd.DataFrame(sentence_vectors_train)
-    X_test = pd.DataFrame(sentence_vectors_test)
+    sentence_vectors_test = sentence_to_vec(df_test, glove_model)
+    sentence_vectors_train = sentence_to_vec(df_train, glove_model)
 
-    y_train = df_train['labels']
-    y_test = df_test['labels']
+    ## ---- make audio fearues ----
+    with open('AudioFeaturesExtraction/train_data.pkl', 'rb') as file:
+        train_audio_dict = pickle.load(file)
 
-    dtrain = xgb.DMatrix(X_train, label=y_train)
-    dtest = xgb.DMatrix(X_test, label=y_test)
+    with open('AudioFeaturesExtraction/test_data.pkl', 'rb') as file:
+        test_audio_dict = pickle.load(file)
+
+    # sample_rate = 44100
+    # audio_feature_train = process_audio_data(train_audio_dict, sample_rate)
+    # audio_feature_test = process_audio_data(test_audio_dict, sample_rate)
+    audio_feature_train = pd.read_csv('audio_features_train.csv')
+    audio_feature_test = pd.read_csv('audio_features_test.csv')
+    audio_feature_train = audio_feature_train.set_index('scene_id')
+    audio_feature_test = audio_feature_test.set_index('scene_id')
+
+    common_ids_train = list(set(audio_feature_train.index).intersection(set(df_train.index)))
+    common_ids_test = list(set(audio_feature_test.index).intersection(set(df_test.index)))
+    y_train = df_train.loc[common_ids_train, 'labels']
+    y_test = df_test.loc[common_ids_test, 'labels']
+
+    # set 3 dfs
+
+    X_train_only_audio = audio_feature_train.loc[common_ids_train]
+    X_test_only_audio = audio_feature_test.loc[common_ids_test]
+    X_train_only_audio = X_train_only_audio.drop(columns='Unnamed: 0')
+    X_test_only_audio = X_test_only_audio.drop(columns='Unnamed: 0')
+
+    X_train_only_text = sentence_vectors_train.loc[common_ids_train]
+    X_test_only_text = sentence_vectors_test.loc[common_ids_test]
+
+    X_train_text_and_audio = pd.concat([X_train_only_audio, X_train_only_text], axis=1)
+    X_test_text_and_audio = pd.concat([X_test_only_audio, X_test_only_text], axis=1)
+
+    dtrain_audio = xgb.DMatrix(X_train_only_audio, label=y_train)
+    dtest_audio = xgb.DMatrix(X_test_only_audio, label=y_test)
+
+    dtrain_text = xgb.DMatrix(X_train_only_text, label=y_train)
+    dtest_text = xgb.DMatrix(X_test_only_text, label=y_test)
+
+    dtrain_text_audio = xgb.DMatrix(X_train_text_and_audio, label=y_train)
+    dtest_text_audio = xgb.DMatrix(X_test_text_and_audio, label=y_test)
 
     # Set up parameters for xgboost
     params = {
@@ -208,12 +256,24 @@ if __name__ == '__main__':
     }
 
     num_boost_round = 100
-    bst = xgb.train(params, dtrain, num_boost_round)
-    preds = bst.predict(dtest)
-    predictions = np.asarray([np.argmax(line) for line in preds])
+    bst_audion = xgb.train(params, dtrain_audio, num_boost_round)
+    preds = bst_audion.predict(dtest_audio)
+    predictions_audio = np.asarray([np.argmax(line) for line in preds])
+
+    bst_text = xgb.train(params, dtrain_text, num_boost_round)
+    preds = bst_text.predict(dtest_text)
+    predictions_text = np.asarray([np.argmax(line) for line in preds])
+
+    bst_text_audio = xgb.train(params, dtrain_text_audio, num_boost_round)
+    preds = bst_text_audio.predict(dtest_text_audio)
+    predictions_text_audio = np.asarray([np.argmax(line) for line in preds])
 
     # Evaluate predictions
+    accuracy_audio = accuracy_score(y_test, predictions_audio)
+    print("Accuracy only audio features: %.2f%%" % (accuracy_audio * 100.0))
 
-    accuracy = accuracy_score(y_test, predictions)
-    print("Accuracy without audio features: %.2f%%" % (accuracy * 100.0))
-    print(classification_report(y_test, predictions))
+    accuracy_text = accuracy_score(y_test, predictions_text)
+    print("Accuracy only text features: %.2f%%" % (accuracy_text * 100.0))
+
+    accuracy_text_audio = accuracy_score(y_test, predictions_text_audio)
+    print("Accuracy text and audio features: %.2f%%" % (accuracy_text_audio * 100.0))
