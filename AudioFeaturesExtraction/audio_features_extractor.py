53e32ef298fce7584088eec8df3a1199e83a1e7c
orlevi <orl@curve.tech>
2024-05-30 15:21:23 +0300

    change filename for consistency & correct

--- AudioFeaturesExtraction/audio_features_extractor.py
+++ AudioFeaturesExtraction/audio_features_extractor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from torch.utils.data import DataLoader
-from AudioFeaturesExtraction.AudioDataset import AudioDataset
+from AudioFeaturesExtraction.audio_dataset import AudioDataset
 import os
 import pickle
 
@@ -97,19 +97,3 @@ class AudioFeaturesExtractor:
         data_loader = self.creating_data_loaders(files_paths, info_file)
         res_dict = self.creating_audio_waveforms(data_loader)
         return res_dict
-
-
-if __name__ == '__main__':
-    train_afe = AudioFeaturesExtractor('../train_audio', '../MELD.Raw/train/train_sent_emo.csv')
-    test_afe = AudioFeaturesExtractor('../test_audio', '../MELD.Raw/test_sent_emo.csv')
-    dev_afe = AudioFeaturesExtractor('../dev_audio', '../MELD.Raw/dev_sent_emo.csv')
-    train_dict = train_afe.run()
-    test_dict = test_afe.run()
-    dev_dict = dev_afe.run()
-
-    with open('train_data.pkl', 'wb') as f:
-        pickle.dump(train_dict, f)
-    with open('test_data.pkl', 'wb') as f:
-        pickle.dump(test_dict, f)
-    with open('dev_data.pkl', 'wb') as f:
-        pickle.dump(dev_dict, f)
