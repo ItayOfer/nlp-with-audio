d4787684ef6db921958bbd598a389c6216b83935
orlevi <orl@curve.tech>
2024-06-09 13:10:27 +0300

    fix fe calling

--- modelling/audio_model.py
+++ modelling/audio_model.py
@@ -27,9 +27,6 @@ if __name__ == '__main__':
     param_grid = {
         'feature_selection__n_features_to_select': [10, 20, 30, 70],
         'model__max_depth': [5, 10, 15]
-        # 'model__max_depth': [5, 10, 15],
-        # 'model__learning_rate': [0.01, 0.05, 0.1],
-        # 'model__n_estimators': [50, 100, 200]
     }
 
     grid = GridSearchCV(pipeline, param_grid, cv=3, scoring='accuracy', verbose=1)
--- modelling/fe_audio_model.py
+++ modelling/fe_audio_model.py
@@ -42,9 +42,7 @@ if __name__ == '__main__':
     param_grid = {
         'feature_selection__n_features_to_select': [10, 20, 30, 70],
         'clustering__n_clusters': [2, 3, 4],
-        'model__max_depth': [5, 10, 15],
-        'model__learning_rate': [0.01, 0.05, 0.1],
-        'model__n_estimators': [50, 100, 200]
+        'model__max_depth': [5, 10, 15]
     }
 
     grid = GridSearchCV(pipeline, param_grid, cv=3, scoring='accuracy', verbose=1)
