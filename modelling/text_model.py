f086292929b5640b6242ffe09341476f6ee8c8c6
orlevi <orl@curve.tech>
2024-06-09 13:02:49 +0300

    merge modelling paths

--- modelling/text_model.py
+++ modelling/text_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
 import lightgbm as lgb
@@ -9,15 +10,17 @@ from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 
+from modelling.scaler import CustomMinMaxScaler
+
 if __name__ == '__main__':
     train_data, test_data, y_train, y_test = utils.get_data()
     text_feature_name = [col for col in train_data.columns if col.startswith('text_feature_')]
     test_meld = pd.read_csv('../MELD.Raw/test_sent_emo.csv')
     X_train = train_data[text_feature_name]
     X_test = test_data[text_feature_name]
-    scaler = MinMaxScaler()
     pipeline = Pipeline([
-        ('scaler', scaler),
+        ('scaler', CustomMinMaxScaler()),
+        ('clustering', KMeans(n_clusters=2)),
         ('feature_selection', RFE(estimator=lgb.LGBMClassifier(n_estimators=50, learning_rate=0.1, random_state=42))),
         ('model',  RandomForestClassifier(random_state=42))
     ])
