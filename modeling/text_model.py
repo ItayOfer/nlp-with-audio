447ba4bb755aa8fc7e44224f6ca7a999f0d26ff3
orlevi <orl@curve.tech>
2024-06-09 12:45:35 +0300

    improve text mpdel

--- modeling/text_model.py
+++ modeling/text_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
 import lightgbm as lgb
@@ -34,17 +35,16 @@ if __name__ == '__main__':
 
     pipeline = Pipeline([
         ('scaler', CustomMinMaxScaler()),
-        ('clustering', ClusterTransformer(cluster_type='kmeans')),
+        ('clustering', KMeans(n_clusters=2)),
         ('feature_selection', RFE(estimator=lgb.LGBMClassifier(n_estimators=50, learning_rate=0.1, random_state=42))),
         ('model',  RandomForestClassifier(random_state=42))
     ])
     param_grid = {
-        'feature_selection__n_features_to_select': [10, 20, 30, 70],
-        'model__max_depth': [5, 10, 15],
+        'feature_selection__n_features_to_select': [10, 15, 20, 30],
         'clustering__n_clusters': [2, 3, 4],
-        # 'model__max_depth': [5, 10, 15],
-        # 'model__learning_rate': [0.01, 0.05, 0.1],
-        # 'model__n_estimators': [50, 100, 200]
+        'model__max_depth': [5, 10, 15],
+        'model__learning_rate': [0.01, 0.05, 0.1],
+        'model__n_estimators': [50, 100, 200]
     }
 
     grid = GridSearchCV(pipeline, param_grid, cv=3, scoring='accuracy', verbose=1)
